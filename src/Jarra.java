3e0acf6e58bfe981f109b4ff7ae709f1ed844ace
martin <28paez@gmail.com>
2016-03-14 21:13:07 +0100

    Final

--- src/Jarra.java
+++ src/Jarra.java
@@ -19,10 +19,22 @@ public class Jarra {//variables de instancia
 	public int cantidad(){
 		return this.cantidad;
 	}
+	public void llenaDesde(Jarra j){
+		while((this.cantidad<this.capacidad)&&(j.cantidad>0)){
+			this.cantidad++;
+			j.cantidad--;
+		} 
+	
+    public String toString(){
+		return ("("+this.capacidad+","+this.cantidad+")");
+    }
+
         
 	
         public void metodoComunitario(){ 
 // Comentario incluido para pruebas 
            System.out.println("Aqui escribimos todos");
          }
+  
+
 
